d27bb140021879efae006de936bf519dba48ff2d
Elvin-Aliyev <elvin.aliyev@live.se>
2022-08-25 10:02:31 +0200

    Unit test buy and sell

--- src/test/java/se/jensensthlm/forexgump/ExchangeCalculatorTest.java
+++ src/test/java/se/jensensthlm/forexgump/ExchangeCalculatorTest.java
@@ -11,12 +11,27 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ExchangeCalculatorTest {
+
+    @InjectMocks
+    ExchangeCalculator exchangeCalculator;
+    @Mock
+    ExchangeProvider exchangeProvider;
+
     @Test
     @DisplayName("Buy 100 GBP")
     public void TestBuy() {
         // Given the exchange rate 10 SEK / GBP
         // To buy 100 GBP
         // You need to pay 1000 SEK
+        var targetCurrency = "GBP";
+        var amount = 100.0;
+        var rate = 10.0;
+        when(exchangeProvider.get("SEK",targetCurrency))
+                .thenReturn(new ExchangeDetails("SEK", targetCurrency, rate));
+
+        var expectedPriceInSek = 1000.0;
+        var actualPriceInSek = exchangeCalculator.calculateBuy(targetCurrency, amount);
+        assertEquals(expectedPriceInSek, actualPriceInSek);
     }
 
     @Test
@@ -25,5 +40,15 @@ public class ExchangeCalculatorTest {
         // Given the exchange rate 10 SEK / GBP
         // Selling 100 SEK
         // Gives you 10 GBP
+        var targetCurrency = "GBP";
+        var amount = 100.0;
+        var rate = 10.0;
+
+        when(exchangeProvider.get("SEK",targetCurrency))
+                .thenReturn(new ExchangeDetails("SEK", targetCurrency, rate));
+
+        var expectedPriceInGbp = 10.0;
+        var actualPriceInSek = exchangeCalculator.calculateSell(targetCurrency, amount);
+        assertEquals(expectedPriceInGbp, actualPriceInSek);
     }
 }
