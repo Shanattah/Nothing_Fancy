da50e772ffda4bc12d53e28fabd8b3ac99987bf5
Vladimir Ristic <94960753+Vladde83@users.noreply.github.com>
2022-08-25 10:03:50 +0200

    unittest calculator

--- src/test/java/se/jensensthlm/forexgump/ExchangeCalculatorTest.java
+++ src/test/java/se/jensensthlm/forexgump/ExchangeCalculatorTest.java
@@ -9,11 +9,28 @@ import org.mockito.Mock;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+
 @ExtendWith(MockitoExtension.class)
 public class ExchangeCalculatorTest {
+
+    @InjectMocks
+    ExchangeCalculator exchangeCalculator; // SUT (System Under Test) This system has dependencies that need to be injected
+
+    @Mock
+    ExchangeProvider exchangeProvider;
     @Test
     @DisplayName("Buy 100 GBP")
     public void TestBuy() {
+        var targetCurrency = "GBP";
+        var amount = 100.0;
+        var rate = 10.0;
+
+        when(exchangeProvider.get("SEK", targetCurrency))
+                .thenReturn(new ExchangeDetails("SEK", targetCurrency, rate));
+
+        var expectedPriceInSek = 1000;
+        var actualPriceInSek = exchangeCalculator.calculateBuy(targetCurrency, amount);
+        assertEquals(expectedPriceInSek, actualPriceInSek);
         // Given the exchange rate 10 SEK / GBP
         // To buy 100 GBP
         // You need to pay 1000 SEK
@@ -22,6 +39,16 @@ public class ExchangeCalculatorTest {
     @Test
     @DisplayName("Sell 100 SEK to get GBP")
     public void TestSell() {
+        var targetCurrency = "GBP";
+        var amount = 100.0;
+        var rate = 10.0;
+
+        when(exchangeProvider.get("SEK", targetCurrency))
+                .thenReturn(new ExchangeDetails("SEK", targetCurrency, rate));
+
+        var expectedPriceInGbp = 10;
+        var actualPriceInSek = exchangeCalculator.calculateSell(targetCurrency, amount);
+        assertEquals(expectedPriceInGbp, actualPriceInSek);
         // Given the exchange rate 10 SEK / GBP
         // Selling 100 SEK
         // Gives you 10 GBP
